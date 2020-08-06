29560550e600e8d95a8a1484a79e9e9499b22695
Elias Moura <d.eliasmoura2@gmail.com>
2020-08-05 23:42:31 -0300

    Update model Book.js

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,9 +1,9 @@
 const { Schema, model } = require("mongoose");
 
 const BookSchema = new Schema({
-  title: { type: String, requiered: true },
-  author: { type: String, requiered: true },
-  isbn: { type: String, requiered: true },
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  isbn: { type: String, requierd: true },
   imagePath: { type: String },
   created_at: { type: Date, default: Date.now },
 });
